b9a471fe4b350e3ba1df4d4f92708c8ddb5f0cbb
MrSwastik592 <swastik592@gmail.com>
2021-11-22 12:49:32 +0530

    added event listeners

--- Tut_1.java
+++ Tut_1.java
@@ -6,6 +6,7 @@ import java.awt.event.*;
 <applet code="Tut_1.class" width=400 height=400></applet>
 */
 public class Tut_1 extends Applet implements ActionListener {
+    Button b1, b2, b3;
 
     public void init() {
         Label l1 = new Label("welcome to java.....!!");
@@ -14,11 +15,13 @@ public class Tut_1 extends Applet implements ActionListener {
         Checkbox ch3 = new Checkbox("Hindi");
         Checkbox ch4 = new Checkbox("Sanskrit");
 
-        Button b1 = new Button("OK");
-        Button b2 = new Button("CANCEL");
-        Button b3 = new Button("RESET");
+        b1 = new Button("OK");
+        b2 = new Button("CANCEL");
+        b3 = new Button("RESET");
 
         b1.addActionListener(this);
+        b2.addActionListener(this);
+        b3.addActionListener(this);
         add(l1);
 
         add(ch1);
@@ -32,8 +35,15 @@ public class Tut_1 extends Applet implements ActionListener {
     }
 
     public void actionPerformed(ActionEvent e) {
-
-        setBackground(Color.RED);
+        if (e.getSource() == b1) {
+            setBackground(Color.RED);
+        }
+        if (e.getSource() == b2) {
+            setBackground(Color.BLUE);
+        }
+        if (e.getSource() == b3) {
+            setBackground(Color.GREEN);
+        }
     }
 
 }
